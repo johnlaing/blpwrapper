c9b80384d1bd531d030da9afa0a1763d4f560aaf
ananelson <>
2008-02-18 17:11:00 +0000

    [project @ 14]  r9892@Anas-iBook:  ana | 2008-02-18 17:09:45 +0000  Several helper functions added to interpret the market, data source and override field relationships.

--- trunk/R/zzz.R
+++ trunk/R/zzz.R
@@ -11,11 +11,20 @@ Future support for platform independent API's is envisioned.")
       assign(".bbfields", bb, ".GlobalEnv")
       message("Contents of bbfields have been stored in .bbfields in the current workspace")
     }
+    ovr <- read.ovr()
+    if(is.null(ovr)){
+      message("I can't find your bloomberg overrides field.")
+    }else{
+      assign(".ovr", ovr, ".GlobalEnv")
+      message("Contents of bbfields.ovr have been stored in .ovr in the current workspace")
+    }
   }
 }
 
 .Last.lib <- function(libpath){
   message("Removing variable .bbfields from the current workspace")
   rm(.bbfields, inherits=TRUE)
+  message ("Removing variable .ovr from the current workspace")
+  rm(.ovr, inherits=TRUE)
 }
 
--- trunk/demo/blpToolsTest.R
+++ trunk/demo/blpToolsTest.R
@@ -1,6 +1,6 @@
-setUp <- function() {
-  multiple.fields <<- c("px_last", "bid", "ask", "NAME", "TRADING_DT_REALTIME")
-}
+# setUp <- function() {
+#   multiple.fields <<- c("px_last", "bid", "ask", "NAME", "TRADING_DT_REALTIME")
+# }
 
 test.is.power.of.two <- function() {
   checkTrue(isPowerOfTwo(2))
@@ -13,6 +13,7 @@ test.field.name.for.single.field <- function() {
   checkEquals(field.name("PX_LAST"), "Last Price")
 }
 test.field.name.for.list.of.fields <- function() {
+  multiple.fields <- c("px_last", "bid", "ask", "NAME", "TRADING_DT_REALTIME")
   checkEquals(
     field.name(multiple.fields), 
     c("Last Price", "Bid Price", "Ask Price", "Name", "Trading Date")
@@ -22,30 +23,28 @@ test.data.type.for.single.field <- function() {
   checkEquals(dataType("PX_LAST"), "double")
 }
 test.data.type.for.list.of.fields <- function() {
+  multiple.fields <- c("px_last", "bid", "ask", "NAME", "TRADING_DT_REALTIME")
   checkEquals(
     dataType(multiple.fields),
-    c("double", "double", "double" "character", "chron")
+    c("double", "double", "double", "character", "chron")
   )
 }
 # cnames <- c("category","category.name","subcategory","subcategory.name",
 #         "field.id","field.name","field.mnemonic","mkt.bitmask",
 #         "data.bitmask","data.type")
 test.category <- function() {
+  multiple.fields <- c("px_last", "bid", "ask", "NAME", "TRADING_DT_REALTIME")
   checkEquals(
     category(multiple.fields),
-    c(10,10,10,185,195)
+    c(195,10,10,185,10)
   )
 }
 test.category.name <- function() {
+  multiple.fields <- c("px_last", "bid", "ask", "NAME", "TRADING_DT_REALTIME")
   checkEquals(
     category.name(multiple.fields),
-    c("Real Time Quotes", "Real Time Quotes", "Real Time Quotes", "Descriptive Info", "Pricing")
+    c("Pricing", "Real Time Quotes", "Real Time Quotes", "Descriptive Info", "Real Time Quotes")
   )
-}
-
-test.commodity <- function() {
-  checkTrue(market.commodity("ED1 Comdty"))
-  checkTrue(!market.commodity("BSC US Equity"))
 }
 
 test.static <- function() {
@@ -54,4 +53,24 @@ test.static <- function() {
 
 test.historical <- function() {
   checkTrue(data.historical("PX_LAST"))
+}
+
+test.what.i.override <- function() {
+  checkEquals(
+    what.i.override("EQY_BETA_OVERRIDE_END_DT"),
+    c(
+    "EQY_BETA_ADJ_OVERRIDABLE"        , "EQY_BETA_RAW_OVERRIDABLE"      , 
+    "EQY_ALPHA_OVERRIDABLE"           , "EQY_CORR_COEF"                 , 
+    "EQY_COEF_DETER_R_SQUARED"        , "EQY_STD_DEV_ERR_OVERRIDABLE"   , 
+    "EQY_BETA_STD_DEV_ERR_OVERRIDABLE", "EQY_BETA_POINTS"               , 
+    "EQY_BETA_T_TEST")
+  )
+}
+
+test.what.overides.me <- function() {
+  checkEquals(
+    what.overrides.me("EQY_CORR_COEF"),
+    c("EQY_BETA_OVERRIDE_START_DT",  "EQY_BETA_OVERRIDE_END_DT",
+      "EQY_BETA_OVERRIDE_REL_INDEX", "EQY_BETA_OVERRIDE_PERIOD")
+    )
 }
