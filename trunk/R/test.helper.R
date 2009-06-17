2be497927b95e8abdf913b6486b37046808dc5f3
ananelson <>
2009-06-17 09:38:44 +0000

    [project @ 37] Fix issue with allBloombergTests not being recognized as a valid test suite.

--- trunk/R/test.helper.R
+++ trunk/R/test.helper.R
@@ -1,3 +1,3 @@
 # To run all unit tests:
 # runTestSuite(allBloombergTests)
-allBloombergTests <- defineTestSuite("All Tests", system.file("runit-tests", package="RBloomberg"), testFileRegexp="Test.R$")
+allBloombergTests <- defineTestSuite("All Tests", dirs=system.file("runit-tests", package="RBloomberg"), testFileRegexp="Test.R$")
