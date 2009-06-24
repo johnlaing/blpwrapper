f83e4d83ef875414adf192a2f5ce490fab1f677f
ananelson <>
2009-06-24 12:53:04 +0000

    [project @ 61] Tweaks to documentation and website.

--- webby/content/examples/historical-calls.R
+++ webby/content/examples/historical-calls.R
@@ -7,13 +7,3 @@ blpGetData(conn, "RYA ID Equity", "PX_LAST", Sys.Date() - 10)
 blpGetData(conn, "RYA ID Equity", "PX_LAST", "2009-01-01", "2009-01-07")
 ### @end
 sink()
-
-
-sink("intraday-overrides.out")
-### @export "intraday-overrides"
-blpGetData(conn, "RYA ID Equity", "PX_LAST", 
-   "2009-01-01", "2009-01-07", 
-   override_fields = c("EQY_FUND_CRNCY"), overrides = c("GBP")
-)
-### @end
-sink()
