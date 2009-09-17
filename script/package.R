eb5b8b563b885d4047a26e66c001273b88d3bb39
Ana Nelson <ana@ananelson.com>
2009-09-17 18:01:53 +0100

    Change manual references to version numbers in build pkg script. Need to automate! Also comment package.R that it is called from elsewhere.

--- script/package.R
+++ script/package.R
@@ -1,3 +1,4 @@
+# This script is called by build_rbloomberg_pkg.bat
 library(tools)
 write_PACKAGES("R/bin/windows/contrib/2.8/", type="win.binary")
 write_PACKAGES("R/bin/windows/contrib/2.9/", type="win.binary")
