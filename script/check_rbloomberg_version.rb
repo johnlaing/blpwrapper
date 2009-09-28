83033847e61b85dfcd4084cbc5a15de9a0be6298
Ana Nelson <ana@ananelson.com>
2009-09-28 10:40:58 +0100

    Use bzr revno for RBloomberg version number.

---
+++ script/check_rbloomberg_version.rb
@@ -0,0 +1,8 @@
+bzr_revno = `bzr revno .`.to_i + 1
+
+rbloomberg_description = File.read("rbloomberg/DESCRIPTION")
+raise "rbloomberg version not found" unless rbloomberg_description =~ /^Version:\s[0-9]+\.[0-9]+-([0-9]+)\s*$/
+rbloomberg_version = $1.to_i
+
+raise "rbloomberg version should be #{bzr_revno} but is #{rbloomberg_version}" unless rbloomberg_version == bzr_revno
+rbloomberg_version
