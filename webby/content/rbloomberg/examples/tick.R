173cea883804458f4793c53e9241c1490a6a02e4
John Laing <john.laing@gmail.com>
2011-08-17 23:33:22 -0400

    Update dates in webby examples

--- webby/content/rbloomberg/examples/tick.R
+++ webby/content/rbloomberg/examples/tick.R
@@ -1,6 +1,6 @@
 library(RBloomberg)
 conn <- blpConnect(log.level = "finest")
-tick(conn, "RYA ID Equity", "TRADE", "2010-09-21 09:00:00.000", "2010-09-21 09:10:00.000")
+tick(conn, "RYA ID Equity", "TRADE", "2011-08-16 09:30:00.000", "2011-08-16 09:40:00.000")
 
-tick(conn, "RYA ID Equity", c("TRADE", "BID_BEST"), "2010-09-21 09:00:00.000", "2010-09-21 09:10:00.000")
+tick(conn, "RYA ID Equity", c("TRADE", "BID_BEST"), "2011-08-16 09:00:00.000", "2011-08-16 09:10:00.000")
 
