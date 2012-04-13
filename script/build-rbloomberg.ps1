8ddafdbd67e92eb22724c0f1502bc52592e227df
John Laing <john.laing@gmail.com>
2012-04-12 20:28:24 -0400

    complete rewrite of build script

--- script/build-rbloomberg.ps1
+++ script/build-rbloomberg.ps1
@@ -1,39 +1,39 @@
-echo "Deleting old files in contrib directories..."
-rm R/src/contrib/*
-rm R/bin/windows/contrib/2.12/*
-rm R/bin/windows/contrib/2.13/*
-rm R/bin/windows64/contrib/2.12/*
-rm R/bin/windows64/contrib/2.13/*
+## Constants
+$R = "C:\R"
+$JDK = "jdk.1.7.0_03"
 
+## Delete old, prepare for new
+rm R/bin -Recurse
 cp java/blpwrapper.jar rbloomberg/inst/java
+$versions = ls -name $R\R-*
 
-# Build 64-bit versions
-$env:JAVA_HOME="C:\Program Files\Java\jdk1.6.0_25"
-echo $env:JAVA_HOME
+## Build 64-bit versions
+$env:JAVA_HOME="C:\Program Files\Java\$JDK"
+foreach ($version in $versions) {
+    $short_version = echo $version | sed 's/\.[0-9]$//'
+    mkdir R/bin/windows64/contrib/$short_version/
 
-& 'C:\Program Files\R\R-2.13.0\bin\x64\R.exe' CMD build --binary rbloomberg
-mv RBloomberg_*.zip R/bin/windows64/contrib/2.13/
+    & "$R\$version\bin\x64\R.exe" CMD build rbloomberg
+    if ($LastExitCode -ne 0) {exit}
+    mv RBloomberg_*.zip R/bin/windows/contrib/$short_version/
 
-& 'C:\Program Files\R\R-2.12.2\bin\x64\R.exe' CMD build --binary rbloomberg
-mv RBloomberg_*.zip R/bin/windows64/contrib/2.12/
+    Rscript -e "require(tools); write_PACKAGES('R/bin/windows64/contrib/$short_version/', type='win.binary')"
+    if ($LastExitCode -ne 0) {exit}
+}
 
+## Now build 32-bit versions
+$env:JAVA_HOME="C:\Program Files (x86)\Java\$JDK"
+foreach ($version in $versions) {
+    $short_version = echo $version | sed 's/\.[0-9]$//'
+    mkdir R/bin/windows/contrib/$short_version/
 
-# Now build 32-bit versions
-$env:JAVA_HOME="C:\Program Files (x86)\Java\jdk1.6.0_25"
-echo $env:JAVA_HOME
+    & "$R\$version\bin\x64\R.exe" CMD build rbloomberg
+    if ($LastExitCode -ne 0) {exit}
+    mv RBloomberg_*.zip R/bin/windows64/contrib/$short_version/
 
-& "C:\Program Files\R\R-2.13.0\bin\i386\R.exe" CMD build --binary rbloomberg
-mv RBloomberg_*.zip R/bin/windows/contrib/2.13/
+    Rscript -e "require(tools); write_PACKAGES('R/bin/windows/contrib/$short_version/', type='win.binary')"
+    if ($LastExitCode -ne 0) {exit}
+}
 
-& "C:\Program Files\R\R-2.12.2\bin\i386\R.exe" CMD build --binary rbloomberg
-mv RBloomberg_*.zip R/bin/windows/contrib/2.12/
-
-
-# Now add package info...
-& 'C:\Program Files\R\R-2.13.0\bin\x64\R.exe' CMD BATCH script\package.R
-
-type script\package.Rout
-del script\package.Rout
-
-# Restore 64 bit Java to default...
-$env:JAVA_HOME="C:\Program Files\Java\jdk1.6.0_25"
+## Restore 64 bit Java to default...
+$env:JAVA_HOME="C:\Program Files\Java\$JDK"
